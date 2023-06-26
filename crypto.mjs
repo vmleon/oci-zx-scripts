8d685f7f6cca570ea0f5c5af931e7fd365e71f09
Victor Martin <vmleon@gmail.com>
2023-06-26 12:39:14 +0200

    fix crypto issues with ssh key gen

--- crypto.mjs
+++ crypto.mjs
@@ -50,10 +50,10 @@ export async function createRSAKeyPair(outputPath = ".") {
 
 export async function createSSHKeyPair(sshPathParam) {
   const defaultSSHPath = path.join(os.homedir(), ".ssh", "id_rsa");
-  const sshPath = sshPathsshPathParam || defaultSSHPath;
+  const sshPath = sshPathParam || defaultSSHPath;
   try {
     await $`ssh-keygen -t rsa -b 4096 -f ${sshPath} -q -N ""`;
-    return await $`cat ${sshPath + ".pub"}`.stdout.trim();
+    return (await $`cat ${sshPath + ".pub"}`).stdout.trim();
   } catch (error) {
     exitWithError(error.stderr);
   }
